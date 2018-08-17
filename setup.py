5a37485639c40f708975f636b5556458b735525a
Tejas Sharma <tejas.s1996@gmail.com>
2018-08-17 09:08:14 -0700

    initial commit with updated code

---
+++ setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='pitchfork',
+    version='v0.1.5',
+    author='Tejas Sharma',
+    author_email='tejas.s1996@gmail.com',
+    packages=['pitchfork', 'tests'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/pitchfork/',
+    license='LICENSE.txt',
+    description='Unofficial API for pitchfork.com reviews, maintained version of pitchfork',
+    long_description=open('README').read(),
+    install_requires=['beautifulsoup4'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP :: Browsers',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Utilities',
+    ],
+)
---
+++ tests/test_pitchfork.py
@@ -0,0 +1,43 @@
+from pytchfork_api.pitchfork import search, Review
+import json
+import unittest
+
+
+class TestReview(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.review = search('mogwai', 'come on')
+
+    def test_review(self):
+        self.assertIsInstance(self.review, Review)
+
+    def test_review_album(self):
+        self.assertEqual(self.review.album(), "Come On Die Young")
+
+    def test_reviev_artist(self):
+        self.assertEqual(self.review.artist(), 'Mogwai')
+
+    def test_review_label(self):
+        self.assertEqual(self.review.label(), 'Chemikal Underground')
+
+    def test_review_year(self):
+        self.assertEqual(self.review.year(), '1999/2014')
+
+    def test_score(self):
+        self.assertEqual(self.review.score(), 8.3)
+
+    def test_editorial(self):
+        self.assertTrue(self.review.editorial().startswith('Though few of their songs contain actual words'))
+
+    def test_review_url(self):
+        self.assertEqual(self.review.url, '/reviews/albums/19466-mogwai-come-on-die-young-deluxe-edition/')
+
+    def test_review_to_json(self):
+        input_dict = self.review._json_safe_dict()
+        output_dict = json.loads(self.review.to_json())
+        for input_key in input_dict.keys():
+            self.assertEqual(output_dict[input_key], input_dict[input_key])
+
+if __name__ == '__main__':
+    unittest.main()
